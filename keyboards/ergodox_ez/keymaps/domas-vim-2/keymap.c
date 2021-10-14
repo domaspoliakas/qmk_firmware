35f882b05ff71ef3a924c2170ee70c974a8fce65
Domas Poliakas <domas@poliakas.dev>
2021-10-14 15:18:48 +0200

    Grave on left hand, " on right thumb

--- keyboards/ergodox_ez/keymaps/domas-vim-2/keymap.c
+++ keyboards/ergodox_ez/keymaps/domas-vim-2/keymap.c
@@ -15,11 +15,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LBRC,                  KC_BSLS, KC_SLSH, KC_Y, KC_U, KC_I, KC_O, KC_P, 
     LT(1,KC_EQL), KC_A, KC_S, KC_D, KC_F, KC_G,                     KC_RBRC, KC_H, KC_J, KC_K, KC_L, KC_COLN, 
     KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LCBR,                 KC_UNDS, KC_RCBR, KC_N, KC_M, KC_COMM, KC_DOT, KC_RSPC, 
-    KC_LCTL, KC_NO, KC_NO, KC_LALT, KC_LGUI,                        KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_ENT, 
+    KC_LCTL, KC_GRV, KC_NO, KC_LALT, KC_LGUI,                        KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_ENT, 
 
 			  KC_F12, KC_NO,                                              TG(4), KC_NO, 
 				KC_PAST, KC_MINS, KC_DQUO,                                  MO(2), KC_CIRC, KC_DLR, 
-				KC_SPC, KC_BSPC, MO(3),                                     KC_RCTL, KC_ESC, KC_ENT
+				KC_SPC, KC_BSPC, MO(3),                                     KC_DQUO, KC_ESC, KC_ENT
 ),
 
 [1] = LAYOUT_ergodox_pretty_80(
