8fa1d741ee57078f1c7cecd9a72156cbe3260bd3
Domas Poliakas <domas@poliakas.dev>
2021-10-19 12:50:26 +0200

    Misc

--- keyboards/ergodox_ez/keymaps/domas-vim-2/keymap.c
+++ keyboards/ergodox_ez/keymaps/domas-vim-2/keymap.c
@@ -7,17 +7,15 @@ enum custom_keycodes {
 #else
     VRSN = SAFE_RANGE,
 #endif
-    FUNCARROW,
-    FORARROW
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT_ergodox_pretty_80(
-    FORARROW, KC_1, KC_2, KC_3, KC_4, KC_5, KC_GRV,      KC_PPLS, KC_QUOT, KC_6, KC_7, KC_8, KC_9, FUNCARROW, 
+    KC_LT, KC_1, KC_2, KC_3, KC_4, KC_5, KC_GRV,                    KC_PPLS, KC_QUOT, KC_6, KC_7, KC_8, KC_9, KC_GT, 
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LBRC,                  KC_BSLS, KC_SLSH, KC_Y, KC_U, KC_I, KC_O, KC_P, 
     LT(1,KC_EQL), KC_A, KC_S, KC_D, KC_F, KC_G,                     KC_RBRC, KC_H, KC_J, KC_K, KC_L, KC_COLN, 
     KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LCBR,                 KC_UNDS, KC_RCBR, KC_N, KC_M, KC_COMM, KC_DOT, KC_RSPC, 
-    KC_LCTL, KC_GRV, KC_NO, KC_LALT, KC_LGUI,                        KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_ENT, 
+    KC_LCTL, KC_GRV, KC_QUES, KC_LALT, KC_LGUI,                        KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_ENT, 
 
 			  KC_F12, KC_NO,                                              TG(4), KC_NO, 
 				KC_PAST, KC_MINS, KC_TILD,                                  MO(2), KC_CIRC, KC_DLR, 
@@ -80,12 +78,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         switch (keycode) {
             case VRSN:
                 SEND_STRING(QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
-                return false;
-            case FUNCARROW:
-                SEND_STRING("=>");
-                return false;
-            case FORARROW:
-                SEND_STRING("<-");
                 return false;
         }
     }
