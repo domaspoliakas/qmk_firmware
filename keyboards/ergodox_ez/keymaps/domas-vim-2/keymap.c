cfa7b3987ab493b0f2bf9e4004ed5ec6690a3052
Domas Poliakas <domas@poliakas.dev>
2021-11-10 18:22:23 +0100

    Cleaned up

--- keyboards/ergodox_ez/keymaps/domas-vim-2/keymap.c
+++ keyboards/ergodox_ez/keymaps/domas-vim-2/keymap.c
@@ -36,9 +36,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT_ergodox_pretty_80(
     KC_LT, KC_1, KC_2, KC_3, KC_4, KC_5, KC_GRV,                    KC_PPLS, KC_QUOT, KC_6, KC_7, KC_8, KC_9, KC_GT, 
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LBRC,                  KC_BSLS, KC_SLSH, KC_Y, KC_U, KC_I, KC_O, KC_P, 
-    LT(1,KC_EQL), KC_A, KC_S, KC_D, KC_F, KC_G,                     KC_RBRC, KC_H, KC_J, KC_K, KC_L, KC_COLN, 
+    LT(1,KC_EQL), KC_A, KC_S, KC_D, KC_F, KC_G,                              KC_RBRC, KC_H, KC_J, KC_K, KC_L, KC_COLN, 
     KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LCBR,                 KC_UNDS, KC_RCBR, KC_N, KC_M, KC_COMM, KC_DOT, KC_RSPC, 
-    KC_LCTL, KC_GRV, KC_QUES, KC_LALT, KC_LGUI,                        KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_ENT, 
+    KC_LCTL, KC_GRV, KC_QUES, KC_LALT, KC_LGUI,                     KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_ENT, 
 
 			  KC_F12, KC_NO,                                              TG(4), KC_NO, 
 				KC_PAST, KC_MINS, KC_TILD,                                  MO(2), KC_CIRC, KC_DLR, 
@@ -46,53 +46,53 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [1] = LAYOUT_ergodox_pretty_80(
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_7, KC_8, KC_9, KC_TRNS, KC_TRNS, 
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                  KC_TRNS, KC_4, KC_5, KC_6, KC_TRNS, KC_TRNS, 
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_1, KC_2, KC_3, KC_TRNS, KC_TRNS, 
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+    _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______, 
+    _______, _______, _______, _______, _______, _______, _______,      _______, _______, KC_7, KC_8, KC_9, _______, _______, 
+    _______, _______, _______, _______, _______, _______,                        _______, KC_4, KC_5, KC_6, _______, _______, 
+    _______, _______, _______, _______, _______, _______, _______,      _______, _______, KC_1, KC_2, KC_3, _______, _______, 
+    _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, 
 
-        KC_TRNS, KC_TRNS,                                               KC_TRNS, KC_TRNS, 
-        KC_TRNS, KC_TRNS, KC_TRNS,                                        KC_TRNS, KC_TRNS, KC_TRNS, 
-        KC_TRNS, KC_TRNS, KC_TRNS,                                        KC_TRNS, KC_0, KC_TRNS
+        _______, _______,                                               _______, _______, 
+        _______, _______, _______,                                      _______, _______, _______, 
+        _______, _______, _______,                                      _______, KC_0, _______
 ),
 
 
 [2] = LAYOUT_ergodox_pretty_80(
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_MUTE, KC_VOLD, KC_VOLU, KC_NO, KC_NO, 
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,               KC_NO, KC_MPRV, KC_MSTP, KC_MPLY, KC_MNXT, KC_NO, 
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+    _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______, 
+    _______, _______, _______, _______, _______, _______, _______,      _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, 
+    _______, _______, _______, _______, _______, _______,                        _______, KC_MPRV, KC_MSTP, KC_MPLY, KC_MNXT, _______, 
+    _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______, 
+    _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, 
                                                                                                                     
-    KC_NO, KC_NO,                                           KC_NO, KC_NO, 
-    KC_NO, KC_NO, KC_NO,                                    KC_TRNS, KC_NO, KC_NO, 
-    KC_NO, KC_NO, KC_NO,                                    KC_NO, KC_NO, KC_NO
+    _______, _______,                                                   _______, _______, 
+    _______, _______, _______,                                          _______, _______, _______, 
+    _______, _______, _______,                                          _______, _______, _______
 
 ),
 
 [3] = LAYOUT_ergodox_pretty_80(
-    KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_NO,        KC_NO, KC_NO, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
-    KC_NO, KC_LT, KC_MINS, KC_EQL, KC_GT, KC_NO, KC_NO,     KC_NO, KC_NO, KC_ASTR, KC_PLUS, KC_DQUO, KC_PIPE, KC_SCLN,
-	  KC_NO, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,        KC_NO, KC_CIRC, KC_SLSH, KC_QUES, KC_UNDS, KC_QUOT, 
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_BSLS, KC_AMPR, KC_GRV, KC_TILD, KC_NO,
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, _______,                _______, _______, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
+    _______, KC_LT, KC_MINS, KC_EQL, KC_GT, _______, _______,           _______, _______, KC_ASTR, KC_PLUS, KC_DQUO, KC_PIPE, KC_SCLN,
+	  _______, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                           _______, KC_CIRC, KC_SLSH, KC_QUES, KC_UNDS, KC_QUOT, 
+    _______, _______, _______, _______, _______, _______, _______,      _______, _______, KC_BSLS, KC_AMPR, KC_GRV, KC_TILD, _______,
+    _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______,
                                                                                                                        
-    KC_NO, KC_NO,                                           KC_NO, KC_NO, 
-    KC_NO, KC_NO, KC_NO,                                    KC_NO, KC_NO, KC_NO, 
-    KC_NO, KC_NO, KC_TRNS,                                  KC_NO, KC_NO, KC_NO
+    _______, _______,                                                   _______, _______, 
+    _______, _______, _______,                                          _______, _______, _______, 
+    _______, _______, _______,                                          _______, _______, _______
 ),
 
 [4] = LAYOUT_ergodox_pretty_80(
-    KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,       KC_NO, KC_NO, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
-    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LSFT,          KC_NO, KC_NO, KC_Y, KC_U, KC_I, KC_O, KC_P,
-    KC_1, KC_A, KC_S, KC_D, KC_F, KC_G,                     KC_NO, KC_H, KC_J, KC_K, KC_L, KC_COLN, 
-    KC_2, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_SPC,             KC_NO, KC_NO, KC_N, KC_M, KC_COMM, KC_DOT, KC_RSFT,
-    KC_3, KC_4, KC_5, KC_6, KC_7,                           KC_NO, KC_RALT, KC_RGUI, KC_APP, KC_RCTL,
+    KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,       _______, _______, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
+    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LSFT,          _______, _______, KC_Y, KC_U, KC_I, KC_O, KC_P,
+    KC_1, KC_A, KC_S, KC_D, KC_F, KC_G,                              _______, KC_H, KC_J, KC_K, KC_L, KC_COLN, 
+    KC_2, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_SPC,             _______, _______, KC_N, KC_M, KC_COMM, KC_DOT, KC_RSFT,
+    KC_3, KC_4, KC_5, KC_6, KC_7,                           _______, KC_RALT, KC_RGUI, KC_APP, KC_RCTL,
                                                                                                                 
-    KC_SPC, KC_BSPC,                                        KC_TRNS, KC_NO,
-    KC_H, KC_J, KC_K,                                       KC_NO, KC_NO, KC_NO,
-    KC_8, KC_9, KC_0,                                       KC_NO, KC_NO, KC_ENT
+    KC_SPC, KC_BSPC,                                        _______, _______,
+    KC_H, KC_J, KC_K,                                       _______, _______, _______,
+    KC_8, KC_9, KC_0,                                       _______, _______, KC_ENT
 ),
 };
 
